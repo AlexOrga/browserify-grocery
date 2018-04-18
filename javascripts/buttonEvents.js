6a113dc894d21a39a832365199c7f0e8094c6f4b
Alex Orga <alexorga.dev@gmail.com>
2018-04-17 19:07:51 -0500

    added cart button which clears all div's

--- javascripts/buttonEvents.js
+++ javascripts/buttonEvents.js
@@ -1,6 +1,7 @@
 const dataGatekeeper = require('./dataGatekeeper');
 
 const goToDepartments = document.getElementById('go-to-departments');
+const goToCart = document.getElementById('go-to-cart');
 const departmentsDiv = document.getElementById('departments');
 const itemsDiv = document.getElementById('items');
 const cartDiv = document.getElementById('cart');
@@ -14,6 +15,15 @@ const departmentsButton = () => {
   });
 };
 
+const cartButton = () => {
+  goToCart.addEventListener('click', () => {
+    departmentsDiv.innerHTML = '';
+    itemsDiv.innerHTML = '';
+    cartDiv.innerHTML = '';
+  });
+};
+
 module.exports = {
   departmentsButton,
+  cartButton,
 };
